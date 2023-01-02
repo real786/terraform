5ca75d596749612a5ef77c3c82363daf0e73f77f
real786 <86429250+real786@users.noreply.github.com>
2023-01-02 12:45:48 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+
+  profile = "default"
+
+  region = "${var.aws_region}"
+
+}
+
+resource "aws_s3_bucket" "onebucket" {
+  bucket = "terrafrom-bucket"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "s3"
+    Environment = "aws"
+    Owner       = "real786"
+
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.onebucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
