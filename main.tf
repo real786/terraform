64fd815cd43822955a3d109cd4d5324730c55f7d
real786 <86429250+real786@users.noreply.github.com>
2023-01-18 16:33:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,12 @@ resource "aws_s3_bucket" "onebucket" {
     }
   }
 }
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.onebucket.id
 
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 
 }
