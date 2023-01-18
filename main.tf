e6be2c121a32ef62c7b8dcabb5079132def477cc
real786 <86429250+real786@users.noreply.github.com>
2023-01-18 17:16:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
+
+  region = "${var.aws_region}"
+
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket"
-  acl    = "public-read"
+resource "aws_s3_bucket" "onebucket" {
+  bucket = "demo-bucket"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.onebucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
