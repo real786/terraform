c9d3f8883577fbfe2d3c40c12bd9e489ee1bb7ea
real786 <86429250+real786@users.noreply.github.com>
2023-01-18 16:41:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,8 @@
 provider "aws" {
-
-  region = "${var.aws_region}"
-
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "public-bucket" {
-  bucket = "terraform-bucket"
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-bucket"
   acl    = "public-read"
-  versioning {
-    enabled = false
-  }
-  tags = {
-    Name        = "s3"
-    Environment = "aws"
-    Owner       = "real786"
-
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
